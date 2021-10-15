0e2efbaa04e6f22e7bcaf3e3a8763499b280b2bf
Bernardo Bevilaqua <bernardo.bfg@hotmail.com>
2021-10-15 12:08:28 -0300

    change size of swipper text

--- src/components/Carousel/slider.tsx
+++ src/components/Carousel/slider.tsx
@@ -13,11 +13,11 @@ interface SliderProps {
 export function Slider({ bg, heading, text, href }: SliderProps) {
   return (
     <SwiperSlide>
-      <Center w={["90vw","90vw", "90vw","1000px","1000px"]} h={["350px", "450px"]} bg={`url(${bg})`} bgSize="cover">
+      <Center w={["90vw","90vw", "90vw","1000px","1000px"]} h={["250px", "450px"]} bg={`url(${bg})`} bgSize="cover">
         <Link href={`continentes/${href}`}>
         <Flex direction="column" align="center" cursor="pointer">
-          <Heading align="center" fontSize={["36","48px"]} color="gray.100">{heading}</Heading>
-          <Text maxWidth="80%" align="center" fontSize={["20px", "24px"]} color="gray.200">{text}</Text>
+          <Heading align="center" fontSize={["28px","48px"]} color="gray.100">{heading}</Heading>
+          <Text maxWidth="80%" align="center" fontSize={["18px", "24px"]} color="gray.200">{text}</Text>
         </Flex>
         </Link>
       </Center>
